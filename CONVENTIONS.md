3a39c6cf5102712203987e6c0a2e1db07bd8e9f2
Bilal Shafi <bilal945@gmail.com>
2017-08-13 14:10:19 +0500

    Edited DB name to avoid conflicts and added CONVENTIONS.md

---
+++ CONVENTIONS.md
@@ -0,0 +1,5 @@
+# Conventions
+
+(This file will list all the conventions followed in system)
+
+Tags will always be case insensitive means Tag=tag=TAG. In database it will always be tag. So from front-end you can pass either.
--- app/database/db.js
+++ app/database/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 module.exports = {
   conn: function(){
-    mongoose.connect('mongodb://localhost/usersdb'),
+    mongoose.connect('mongodb://localhost/SocialNetwork'),
     mongoose.Promise = global.Promise;
   }
 }
